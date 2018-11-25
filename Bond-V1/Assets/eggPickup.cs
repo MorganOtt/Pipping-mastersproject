0dfa5ef033c3f5ff193149737bb77e372cb42947
MorganOtt <morgan.a.ott@gmail.com>
2018-11-25 18:55:07 -0500

    Initial Commit

---
+++ Bond-V1/Assets/eggPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eggPickup : MonoBehaviour {
+	public GameObject ObjectTracker;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("Egg Picked Up!");
+		ObjectTracker.GetComponent<Animation>().CrossFade("eggShake");
+	}
+}
