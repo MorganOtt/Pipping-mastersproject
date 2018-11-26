32215eb9a61fa73e5177ee384dce91bb7f0fe3b8
Morgan Ott <morgan.a.ott@gmail.com>
2018-11-26 16:58:20 -0500

    3rd person controller, audio, object switch

---
+++ Bond-V1/Assets/createEgg.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createEgg : MonoBehaviour {
+    public Transform dragon;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("touching" + other.name);
+
+        if (other.name == "audio3")
+        {
+
+            Debug.Log("It's a hit");
+            //// Instantiate the projectile at the position and rotation of this transform
+            //Rigidbody clone;
+            //clone = Instantiate(dragon, transform.position, transform.rotation);
+
+            Instantiate(dragon, new Vector3(0, 2, 0), Quaternion.identity);
+
+        }
+    }
+
+
+}
---
+++ Bond-V1/Assets/playAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAudio : MonoBehaviour {
+
+    public AudioClip egg2;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = egg2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("detected");
+        GetComponent<AudioSource>().Play();
+    }
+}
